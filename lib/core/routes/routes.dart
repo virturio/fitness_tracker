98089783d71c3f2812d03ee135aa4e62439befd1
muhammad rio <muhammad.rioperm@gmail.com>
2024-09-27 23:51:17 +0700

    [Features] Add gender selection page

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,4 +1,4 @@
-import 'package:fitness_tracker/view/setup/setup/pages/setup_page.dart';
+import 'package:fitness_tracker/view/setup/gender_select/pages/gender_selection_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,7 +12,7 @@ final appRouter = GoRouter(
       path: "/",
       pageBuilder: (context, state) => const MaterialPage(
         key: _pageKey,
-        child: SetupPage(),
+        child: GenderSelectionPage(),
       ),
     ),
   ],
--- lib/view/auth/widget/auth_appbar.dart
+++ lib/view/auth/widget/auth_appbar.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:svg_flutter/svg.dart';
 
 class AuthAppbar extends StatelessWidget implements PreferredSizeWidget {
-  const AuthAppbar({super.key, required this.titleText});
+  const AuthAppbar({super.key, this.titleText = '', this.leadingText});
   final String titleText;
+  final String? leadingText;
 
   @override
   Widget build(BuildContext context) {
@@ -14,26 +15,40 @@ class AuthAppbar extends StatelessWidget implements PreferredSizeWidget {
       fontSize: 20,
       fontWeight: FontWeight.bold,
     );
+
+    TextStyle leadingTextStyle = const TextStyle(
+      color: AppColors.limeGreen,
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+    );
     return AppBar(
-        title: Text(titleText),
-        titleTextStyle: titleStyle,
-        centerTitle: true,
-        leadingWidth: 38,
-        leading: Padding(
-          padding: const EdgeInsets.only(
-            left: 32,
-            top: 8,
-          ),
-          child: InkWell(
-            onTap: () => Navigator.maybePop(context),
-            splashColor: AppColors.limeGreen,
-            child: Ink(
-              child: SvgPicture.asset(
-                AppVectors.arrow,
-              ),
+      title: Text(titleText),
+      titleTextStyle: titleStyle,
+      centerTitle: true,
+      leadingWidth: 84,
+      leading: Padding(
+        padding: const EdgeInsets.only(
+          left: 32,
+        ),
+        child: Row(
+          children: [
+            SvgPicture.asset(
+              AppVectors.arrow,
+              width: 12,
+              height: 12,
             ),
-          ),
-        ));
+            if (leadingText != null)
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  leadingText!,
+                  style: leadingTextStyle,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
